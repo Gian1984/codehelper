66c99cd4bac3b48044be36a717d6502913bca31b
gianlucatiengo <gl.tiengo@gmail.com>
2025-03-27 11:53:36 +0100

    Working on: set up boilerplate and needed services

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/sitemap'
+  ],
+
+  // @ts-ignore — PhpStorm/Volar doesn't recognize this, but Nuxt will
+  sitemap: {
+    siteUrl: 'https://codehelper.me',
+    sitemapName: 'sitemap.xml',
+    gzip: true,
+    routes: [
+      '/',        // Home page
+      '/about'    // About page
+    ]
+  }
 })
