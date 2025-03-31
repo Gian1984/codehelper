fe61621feafc3b47ad16440127267a5154e43d6f
Gian1984 <gl.tiengo@gmail.com>
2025-03-31 22:24:16 +0200

    adding favicon and some img

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,11 @@
       <div class="aspect-1097/845 w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
     </div>
     <div class="mx-auto max-w-2xl text-center">
+      <img
+          class="aspect-square w-36 rounded-2xl mx-auto mb-4"
+          src="/images/codehelper_logo_500_500_no_bg.webp"
+          alt="Gianluca Tiengo"
+      />
       <h1 class="text-5xl font-semibold tracking-tight text-white sm:text-7xl">CodeHelper</h1>
       <p class="mt-6 text-lg font-medium text-gray-400 sm:text-xl">A modern toolbox for developers. Format, convert, generate, and optimize your work with simple, free tools for the web.</p>
     </div>
