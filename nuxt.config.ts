77bde22edc2202f775e5fdcc0eb2e7c8acd2c34c
gianlucatiengo <gl.tiengo@gmail.com>
2025-04-02 14:28:44 +0200

    Prepare nuxt to build a static website

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  ssr: false, // disables server rendering, full static
+
+  nitro: {
+    preset: 'static' // makes sure Nuxt generates a static site
+  },
+
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/sitemap'
